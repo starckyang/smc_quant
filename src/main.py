dde4fea42f49d14ed35730863f0e22ccaffeb51e
starck <starck.catwarrior@gmail.com>
2024-06-20 22:53:31 +0800

    add requirements.txt

--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ import os
 from backtesting import Backtest
 from src.strategy import OrderBlockStrategy
 
-data_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'ETHUSDT_testing_15m.pkl')
+data_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'ETHUSDT_training_15m.pkl')
 
 if __name__ == "__main__":
     # Load your data
     with open(data_path, "rb") as f:
         data = pickle.load(f)
         # Convert "Time" column to datetime and set it as index
-    data["Time"] = pd.to_datetime(data["Time"])
-    data.set_index("Time", inplace=True)
+    # data["Time"] = pd.to_datetime(data["Time"])
+    # data.set_index("Time", inplace=True)
     # Run the backtest
     bt = Backtest(data, OrderBlockStrategy, cash=1000000)
     stats = bt.run()
